1a12b4e468868b4a76a00a2e9d2ec932f5e523c8
lllsss14 <1579188215@qq.com>
2022-05-16 19:37:11 +0800

    更改

--- FistProject/FistProject/ViewController.swift
+++ FistProject/FistProject/ViewController.swift
@@ -25,5 +25,11 @@ class ViewController: UIViewController {
         alterConrtoller.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alterConrtoller, animated: true, completion: nil)
     }
+    
+    @IBAction func othershowMessage(sender: UIButton) {
+        let alterConrtoller = UIAlertController(title: "Welcome to my second App", message: "no more", preferredStyle: UIAlertController.Style.alert)
+        alterConrtoller.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alterConrtoller, animated: true, completion: nil)
+    }
 }
 
